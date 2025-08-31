2de49a43f8a536100bff0e20e2413057d5b4d3ff
Stanley Ojadovwa <stanley.warri@gmail.com>
2025-08-31 10:59:10 +0100

    Fix seeding admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            BookCommunitySeeder::class
+           // BookCommunitySeeder::class
         ]);
     }
 }
